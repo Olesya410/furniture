1ea7817a5ee19014110ed758e7c29c89a7ec9e9d
Olesya410 <jin204918@gmail.com>
2026-02-27 12:10:49 +0400

    Update CatalogController.php

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -10,24 +10,21 @@ use App\Models\Product;
 
 class CatalogController extends Controller
 {
-    // Метод общего списка товаров
     public function index()
     {
-        $products = Catalog::with('products')->get(); // Предполагаем, что у каждой категории есть связанные товары
+        $products = Catalog::with('products')->get(); 
         return view('catalog', compact('products'));
     }
 
-    // Товары по конкретной категории
     public function showCategories($id)
     {
         $category = Product::where('catalog_id', $id)->get(['id', 'img', 'img_name', 'price', 'price_old', 'product_size', 'description', 'name', 'catalog_id']);
         return view('category', compact('category'));
     }
 
-    // Детали отдельного товара
     public function show($id)
     {
-        $product = Product::findOrFail($id); // находим конкретный товар
+        $product = Product::findOrFail($id);
         return view('product', compact('product'));
     }
 }
