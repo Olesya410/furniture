799da64cad906e4efea47c9549a18cfb536db97b
Olesya410 <jin204918@gmail.com>
2026-02-27 12:14:18 +0400

    Update SubscriptionController.php

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -8,13 +8,8 @@ class SubscriptionController extends Controller
 {
     public function store(Request $request)
     {
-        // Валидируем введённый email
         $request->validate(['email' => 'required|email|unique:subscriptions']);
-
-        // Создаём новую запись в базе данных
         Subscription::create(['email' => $request->input('email'), 'subscribed_at' => now()]);
-
-        // Возвращаемся обратно на страницу с уведомлением
         return back()->with('success_message', 'Спасибо за подписку!');
     }
 }
