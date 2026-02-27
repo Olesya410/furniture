85e5629c30f2e47a7c8b0498dd35493f76e6f554
Olesya410 <jin204918@gmail.com>
2026-02-27 12:17:01 +0400

    Update Order.php

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,29 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
-
-    // Укажите поля, которые можно массово заполнять
+    
     protected $fillable = [
         'user_id',
         'name',
         'email',
         'phone',
         'address',
-        // добавьте другие поля по необходимости
     ];
 
-    // Связь с позициями заказа
     public function items()
     {
         return $this->hasMany(CartItem::class);
     }
 
-    // Связь с пользователем
     public function user()
     {
         return $this->belongsTo(User::class);
     }
-    // В модели Order.php
     public function orderItems()
     {
         return $this->hasMany(OrderItem::class, 'order_id');
